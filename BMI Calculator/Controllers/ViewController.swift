0fd476264bce2ef3d2f9d709b70c6d9a8abae8e7
safarovsafar <safarov.safar@hotmail.com>
2020-08-03 04:03:42 +0800

    BMI-calculator-ios13

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -4,21 +4,35 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("hello world!")
     }
     
    
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        heightLabel.text = String(format: "%.2f", sender.value)
-        
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
     }
     
 
     @IBAction func weightSliderChanged(_ sender: UISlider) {
-        weightLabel.text = String(Int(sender.value))
+        let weight = String(format: "%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
     }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+//        what does pow, first value is the value that you want to x, second is the value for how many times you want to multiply it
+        let bmi = weight / pow(height, 2)
+    }
+    
 }
 
