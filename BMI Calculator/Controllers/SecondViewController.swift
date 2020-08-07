3193574376887b7c25d4be112273e887e39c8a21
safarsafarov <safarov.safar@hotmail.com>
2020-08-08 01:10:33 +0800

    BMI

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Safar Safarov on 2020/8/8.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewConroller: UIViewController {
+    override func viewDidLoad() {
+         super.viewDidLoad()
+         // Do any additional setup after loading the view.
+    }
+}
--- BMI Calculator/Controllers/secondViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  secondViewController.swift
-//  BMI Calculator
-//
-//  Created by Safar Safarov on 2020/8/7.
-//  Copyright © 2020 Angela Yu. All rights reserved.
-//
-
-import Foundation
